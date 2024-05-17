99607b1190949330f8e464c2d51d871fe225a00d
“ievatm” <Ieva.atmine@gmail.com>
2024-05-18 00:13:26 +0200

    add computers choice function to crate computers choice added

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+
+// function to get computers choice 
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+
+// The logic to get the human choice
+
+function getHumanChoice() {
+    
+}
+// Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+
+// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+// Hint: Use the prompt method to get the user’s input.
+// Test what your function returns by using console.log.
+
+
+
+
+
+
+
+
